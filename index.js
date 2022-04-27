9b773226f4538656bc8555a4aa63d5b5028d6cac
n-double-o <jibinjtomas@gmail.com>
2022-04-27 10:12:17 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* 
+1. Array to hold services requested
+2. Buttons to add services to array
+3. Place to display data from array - updated every time the array changes
+4. Don't charge > once for some services
+5. Total cost stays updated
+6. Button to "send invoice" (reset)
+Stetch: remove items after adding
+*/
